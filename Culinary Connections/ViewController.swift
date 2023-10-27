3b286ad59bf5e00cd78b8d3fadf978a9744f44ff
Vahé Krikorian <vahekrikorian@macbook-pro.home>
2023-10-27 18:30:48 +0200

    init

---
+++ Culinary Connections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Culinary Connections
+//
+//  Created by Vahé Krikorian on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
